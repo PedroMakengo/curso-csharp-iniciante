ee681397c185aed26071fc88727c0e23e1fccce7
PedroMakengo <pedromakengomiguel@gmail.com>
2023-07-16 07:44:28 +0100

    Adicionando os projetos

---
+++ ProjetoCursoCanalHenrique/OlaSouCSharp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite o seu nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Bem-vindo {0}", nome);
---
+++ demo06/Program.cs
@@ -0,0 +1,33 @@
+﻿DateTimeExemplo();
+
+static void DateTimeExemplo()
+{
+  DateTime dataInicio = new DateTime(2019, 10, 01); // Data
+  DateTime hoje = DateTime.Today; // Hoje
+  DateTime agora = DateTime.Now; // Agora
+
+  // Console.WriteLine("Data de Início " + dataInicio.ToString("dd/MM/yyyy HH:mm:ss"));
+  // Console.WriteLine("" + hoje);
+
+
+  DayOfWeek diaDaSemana = hoje.DayOfWeek;
+  Console.WriteLine("Dia da semana " + diaDaSemana);
+
+  // DateTime dataFim = hoje.AddDays(10);
+  // Console.WriteLine("Data de Fim " + dataFim.ToString("dd/MM/yyyy HH:mm:ss"));
+
+  // bool ehorarioVeracao = agora.IsDaylightSavingTime();
+
+  // DateOnly dateOnly = new DateOnly(2023, 19, 02);
+
+  // DateOnly dateOnlyFromDateNow = DateOnly.FromDateTime(DateTime.Now);
+
+  // DateTime horaEntrada  = DateTime.Now; 
+  // Console.WriteLine("Hora de Entrada" + horaEntrada);
+  // TimeSpan horarioTrabalho = new TimeSpan(8, 0, 0);
+  // DateTime horaSaida = horaEntrada.Add(horarioTrabalho);
+  // Console.WriteLine("Hora de Saída " + horaSaida);
+
+  // TimeOnly horaEntradaTimeOnly = new TimeOnly(22, 19, 24);
+  // Console.WriteLine("Hora de entrada " + horaEntradaTimeOnly);
+}
